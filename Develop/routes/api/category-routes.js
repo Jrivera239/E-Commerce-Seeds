0229bf8badb934bfa6c891379f5b475ae7d99cbf
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-09-26 14:14:13 -0400

    adding category params and create

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
     include: [
       {
         model:Product,
-        attributes["id", "prudct_name", "price", "stock"],
+        attributes:["id", "prudct_name", "price", "stock"],
       },
     ],
   })
@@ -19,12 +19,45 @@ router.get('/', (req, res) => {
       res.status(500).json({ message: err });
     });
 });
-  // be sure to include its associated Products
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock"],
+      },
+    ],
+  })
+    .then((dbCategoryData) => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: "Category not Found" });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
+
+router.post("/", (req, res) => {
+  Category.create({
+    category_name: req.body.category_name,
+  })
+    .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
 
 router.post('/', (req, res) => {
   // create a new category
