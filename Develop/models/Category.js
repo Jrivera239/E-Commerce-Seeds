066628197b9366068a64f6f0ba71970ba8f99585
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-09-27 18:33:40 -0400

    update category

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -15,7 +15,6 @@ Category.init(
     type: DataTypes.STRING,
     allowNull: false,
   },
-  
 },
 {
   sequelize,
@@ -23,7 +22,6 @@ Category.init(
   freezeTableName: true,
   underscored: true,
   modelName: 'category',
-}
-);
+});
 
 module.exports = Category;
